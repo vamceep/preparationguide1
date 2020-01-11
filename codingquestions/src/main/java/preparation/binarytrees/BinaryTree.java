63584d96bf63bcf123b597ab0fe822e90a05d138
ssurampu <sridhar_sds@yahoo.co.in>
2020-01-12 02:34:13 +0530

    code changes

--- codingquestions/src/main/java/preparation/binarytrees/BinaryTree.java
+++ codingquestions/src/main/java/preparation/binarytrees/BinaryTree.java
@@ -1,14 +1,20 @@
 package preparation.binarytrees;
 
 
+import preparation.linkedlists.LinkedList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
 public class BinaryTree {
-    private BinaryTreeNode root = null;
+    private static BinaryTreeNode root = null;
 
     public BinaryTreeNode getNode(int val) {
         return new BinaryTreeNode(val);
     }
 
-    public int lca(int n1, int n2) {
+    public static int lca(int n1, int n2) {
         BinaryTreeNode lca = lcaInternal(root, n1, n2);
         if(lca != null) {
             return lca.val;
@@ -16,7 +22,7 @@ public class BinaryTree {
             throw new NullPointerException("No lca found");
         }
     }
-    private BinaryTreeNode lcaInternal(BinaryTreeNode root, int n1, int n2) {
+    private static BinaryTreeNode lcaInternal(BinaryTreeNode root, int n1, int n2) {
         if(root == null) {
             return root;
         }
@@ -46,7 +52,7 @@ public class BinaryTree {
         printInorder(root.right);
     }
 
-    public void createSampleTree() {
+    public static void createSampleTree() {
         root = new BinaryTreeNode(5);
         root.left = new BinaryTreeNode(8);
         root.right = new BinaryTreeNode(2);
@@ -66,5 +72,48 @@ public class BinaryTree {
 
 
         root.right.left.left = new BinaryTreeNode(33);
+    }
+
+    public static boolean findPath(BinaryTreeNode root, int val, List<BinaryTreeNode> path) {
+        if(root == null) {
+            return false;
+        }
+        path.add(root);
+        if(root.val == val) {
+            return true;
+        }
+        if(root.left != null && findPath(root.left, val, path)
+            || (root.right != null && findPath(root.right, val, path))){
+            return true;
+        }
+
+        path.remove(root);
+        return false;
+    }
+    public static BinaryTreeNode findLCA(BinaryTreeNode root, int v1, int v2) {
+        List<BinaryTreeNode> path1 = new ArrayList<>();
+        List<BinaryTreeNode> path2 = new ArrayList<>();
+        if(!findPath(root,v1, path1) || !findPath(root, v2, path2)) {
+            return null;
+        }
+        int i = 0;
+        for(i=0; i < path1.size() && i < path2.size();i++) {
+            if(path1.get(i).val != path2.get(i).val) {
+                break;
+            }
+        }
+        return path1.get(i-1);
+    }
+    public static void main(String[] args) {
+        createSampleTree();
+        BinarySearchTree.printInorder(root);
+        int v1 = 33;
+        int v2 = 25;
+        BinaryTreeNode result = findLCA(root, v1, v2);
+        if(result != null) {
+            System.out.println("\n LCA of " + v1 + " " + v2 + " is : " + result.val);
+        }else {
+            System.out.println("\n No LCA found for " + v1 + " " + v2);
+        }
     }
 }
