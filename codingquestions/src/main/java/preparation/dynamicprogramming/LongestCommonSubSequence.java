20dbeb2c030bae802395855ba1b2e61aef338f35
ssurampu <sridhar_sds@yahoo.co.in>
2020-02-05 11:34:17 +0530

    code changes

---
+++ codingquestions/src/main/java/preparation/dynamicprogramming/LongestCommonSubSequence.java
@@ -0,0 +1,55 @@
+package preparation.dynamicprogramming;
+
+import java.util.Arrays;
+
+public class LongestCommonSubSequence {
+    /**
+     * Longest Common Subsequence | DP using Memoization
+     * Given two strings s1 and s2, the task is to find the length of longest common subsequence present in both of them.
+     *
+     * Examples:
+     *
+     * Input: s1 = “ABCDGH”, s2 = “AEDFHR”
+     * Output: 3
+     * LCS for input Sequences “AGGTAB” and “GXTXAYB” is “GTAB” of length 4.
+     *
+     *
+     *
+     * Input: s1 = “striver”, s2 = “raj”
+     * Output: 1
+     * https://www.geeksforgeeks.org/longest-common-subsequence-dp-using-memoization/
+     */
+
+    public static int longestCommonSubSequenceLength(String x, String y, int m, int n, int[][] dp) {
+        if(m==0 || n==0) {
+            return 0;
+        }
+
+        if(dp[m-1][n-1]!= -1) {
+            return dp[m-1][n-1];
+        }
+        if(x.charAt(m-1) == y.charAt(n-1)) {
+            dp[m-1][n-1] = 1 + longestCommonSubSequenceLength(x, y, m-1, n-1, dp);
+            return dp[m-1][n-1];
+        }else {
+            dp[m-1][n-1] = Math.max(longestCommonSubSequenceLength(x,y, m, n-1, dp),
+                    longestCommonSubSequenceLength(x,y,m-1,n,dp));
+            return dp[m-1][n-1];
+        }
+    }
+    public static void main(String[] args) {
+        int max = 10000;
+//        String x = "AGGTAB";
+//        String y = "GXTXAYB";
+        String x = "abbcb";
+        String y = "acdebbec";
+        int m = x.length();
+        int n = y.length();
+        int[][] dp = new int[m][max];
+        // assign -1 to all positions
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        System.out.println("lcss length: " + longestCommonSubSequenceLength(x, y, m,n,dp));
+    }
+}
