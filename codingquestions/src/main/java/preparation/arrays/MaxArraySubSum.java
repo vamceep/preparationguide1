931658b3667766428d2220924fd88b4c89422aeb
ssurampu <sridhar_sds@yahoo.co.in>
2020-02-06 14:56:12 +0530

    code changes

--- codingquestions/src/main/java/preparation/arrays/MaxArraySubSum.java
+++ codingquestions/src/main/java/preparation/arrays/MaxArraySubSum.java
@@ -2,6 +2,23 @@ package preparation.arrays;
 
 public class MaxArraySubSum {
 
+    /**
+     * Size of The Subarray With Maximum Sum
+     * An array is given, find length of the subarray having maximum sum.
+     *
+     * Examples :
+     *
+     * Input :  a[] = {1, -2, 1, 1, -2, 1}
+     * Output : Length of the subarray is 2
+     * Explanation: Subarray with consecutive elements
+     * and maximum sum will be {1, 1}. So length is 2
+     *
+     * Input : ar[] = { -2, -3, 4, -1, -2, 1, 5, -3 }
+     * Output : Length of the subarray is 5
+     * Explanation: Subarray with consecutive elements
+     * and maximum sum will be {4, -1, -2, 1, 5}.
+     * same as : Largest Sum Contiguous Subarray
+     */
     public static int maxArraySubSumWithNegativeValues(int[] arr) {
         int n = arr.length;
         int local_max = 0;
