65316e4fc8f741f866acdcf6fa1e6f3715c2f384
sridhar <sridhar.surampudi@wdc.com>
2020-03-19 23:29:17 +0530

    merged as single class

--- codingquestions/src/main/java/preparation/threads/PrintEvenOdd.java
+++ codingquestions/src/main/java/preparation/threads/PrintEvenOdd.java
@@ -3,6 +3,35 @@ package preparation.threads;
 public class PrintEvenOdd {
     public static int NUMBERS_UPTO = 10;
 
+    static public class OddEvenNumberPrint implements Runnable {
+        static int number = 1;
+        public int upto = 0;
+        public int remainder = 0;
+        static Object lock = new Object();
+
+        public OddEvenNumberPrint(int upto, int remainder) {
+            this.upto = upto;
+            this.remainder = remainder;
+        }
+
+        @Override
+        public void run() {
+            while (number < upto) {
+                synchronized (lock) {
+                    while (number % 2 != remainder) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println(Thread.currentThread().getName() + " " + number);
+                    number++;
+                    lock.notifyAll();
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         OddEvenNumberPrint evenNumberPrint = new OddEvenNumberPrint(10, 0);
         OddEvenNumberPrint oddNumberPrint = new OddEvenNumberPrint(10, 1);
