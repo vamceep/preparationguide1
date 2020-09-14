8861250b72fa3ed2b30482b3fc4d558150d8bcda
sridhar <sridhar.surampudi@wdc.com>
2020-09-14 08:02:55 +0530

    added a new test

--- codingquestions/src/main/java/preparation/binarytrees/BinaryTreeDiameter.java
+++ codingquestions/src/main/java/preparation/binarytrees/BinaryTreeDiameter.java
@@ -2,9 +2,20 @@ package preparation.binarytrees;
 
 public class BinaryTreeDiameter {
     /**
-     *
+     * Diameter of a Binary Tree
+     * The diameter of a tree (sometimes called the width) is the number of nodes on the longest path between
+     * two end nodes. The diagram below shows two trees each with diameter nine, the leaves that form the ends
+     * of a longest path are shaded (note that there is more than one path in each tree of length nine, but no
+     * path longer than nine nodes).
      *
      * https://www.geeksforgeeks.org/diameter-of-a-binary-tree/
+     *
+     * The diameter of a tree T is the largest of the following quantities:
+     *
+     * * the diameter of T’s left subtree
+     * * the diameter of T’s right subtree
+     * * the longest path between leaves that goes through the root of T
+     * (this can be computed from the heights of the subtrees of T)
      */
     public static class Node {
         int val;
